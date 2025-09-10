c2384c2fec75cbfb58e8a4451381bc9950ecc93c
Veinal <veinalsequeira5229@gmail.com>
2025-09-10 22:52:15 +0530

    added turn server

--- src/lib/webrtc.ts
+++ src/lib/webrtc.ts
@@ -159,10 +159,16 @@ export function useWebRTC(): UseWebRTCReturn {
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun1.l.google.com:19302" },
         { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun3.l.google.com:19302" },
-        { urls: "stun:stun4.l.google.com:19302" },
-        { urls: "stun:stun.services.mozilla.com:3478" },
-        { urls: "stun:stun.stunprotocol.org:3478" },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
       ],
     });
     peerConnectionRef.current = pc;
