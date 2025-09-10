1a54338c01fcc73a1fdcfaea6e731bff4121f868
Veinal <veinalsequeira5229@gmail.com>
2025-09-10 22:37:20 +0530

    added few more stun servers

--- src/lib/webrtc.ts
+++ src/lib/webrtc.ts
@@ -157,7 +157,12 @@ export function useWebRTC(): UseWebRTCReturn {
     const pc = new RTCPeerConnection({
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:global.stun.twilio.com:3478" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.services.mozilla.com:3478" },
+        { urls: "stun:stun.stunprotocol.org:3478" },
       ],
     });
     peerConnectionRef.current = pc;
